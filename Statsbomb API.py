4bdb678118adf8fa80f71a769f6c220be091c761
Sachindra <47586567+therealspark9@users.noreply.github.com>
2022-10-11 23:59:40 +0530

    Create Statsbomb API.py

---
+++ Statsbomb API.py
@@ -0,0 +1,7 @@
+from statsbombpy import sb
+
+#Display all football match results of English Premier League 2022/23 so far
+print(sb.matches(competition_id=2, season_id=44))
+
+#Display all events that are recorded by Statsbomb data
+print(events.columns)
